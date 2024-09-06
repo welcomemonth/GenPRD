922dbde0f1c493cb63f8b868cfe354d94e8f5ab5
zhengyu <midrain1999@gmail.com>
2024-09-06 22:35:51 +0800

    当token失效，强制刷新页面到首页

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -3,5 +3,13 @@ import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req).pipe(catchError((err) => throwError(() => err.error)));
+  return next(req).pipe(
+    catchError((err) => {
+      if (err.status === 401) {
+         // 路由到登录页面
+         window.location.href = "/";
+      }
+      return throwError(() => err.error);
+    })
+  );
 };
