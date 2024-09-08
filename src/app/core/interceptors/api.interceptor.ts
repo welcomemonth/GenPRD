376daf99e2b3727f5f087062b1198717213621a8
zhengyu <midrain1999@gmail.com>
2024-09-08 19:21:16 +0800

    添加本地接口调用，以及prd.service

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -2,6 +2,6 @@ import { HttpInterceptorFn } from "@angular/common/http";
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   // const apiReq = req.clone({ url: `https://api.realworld.io/api${req.url}` });
-  const apiReq = req.clone({ url: `http://localhost:8000/api${req.url}` });
+  const apiReq = req.clone({ url: `http://192.168.0.102:8000/api${req.url}` });
   return next(apiReq);
 };
---
+++ src/app/features/article/services/prd.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({ providedIn: "root" })
+export class PrdService {
+  constructor(private readonly http: HttpClient) {}
+
+  create_prd(): Observable<string> {
+    return this.http
+      .post<string>("/prd", { title: "test", detail: "test" })
+      .pipe(map((data) => data));
+  }
+
+
+}
