3e0611e65e48424d11ea302d730d65471e4ba6a8
zhengyu <midrain1999@gmail.com>
2024-09-27 13:08:44 +0800

    删除home文件中一些遗留的无关字段

--- src/app/features/article/pages/home/home.component.ts
+++ src/app/features/article/pages/home/home.component.ts
@@ -6,7 +6,6 @@ import {
   ElementRef
 } from "@angular/core";
 import { Router } from "@angular/router";
-import { TagsService } from "../../services/tags.service";
 import { ArticleListConfig } from "../../models/article-list-config.model";
 import { AsyncPipe, NgClass, NgForOf } from "@angular/common";
 import { ArticleListComponent } from "../../components/article-list.component";
@@ -39,59 +38,12 @@ import { PrdService } from "../../services/prd.service";
 export default class HomeComponent implements OnInit {
 
   isAuthenticated = false;
-  test_text = "";
-  listConfig: ArticleListConfig = {
-    type: "all",
-    filters: {},
-  };
-  tags$ = inject(TagsService)
-    .getAll()
-    .pipe(tap(() => (this.tagsLoaded = true)));
-  tagsLoaded = false;
   destroyRef = inject(DestroyRef);
   markdownContent: string = `
 你好！我是你的AI产品小帮手，我可以帮你撰写一份PRD\n
 请在左侧输入功能名和需求描述，点击生成就可以!
 `;
-  structure = `
-## 系统结构图
-\`\`\`mermaid
-graph TD
-    A[学生管理系统] --> B[学生模块]
-    A --> C[课程模块]
-    A --> D[教师模块]
-    A --> E[成绩模块]
-    A --> F[班级模块]
-
-    B --> B1[学生信息管理]
-    B --> B2[学生注册]
-    B --> B3[学生查询]
-
-    C --> C1[课程信息管理]
-    C --> C2[课程安排]
-    C --> C3[课程查询]
-
-    D --> D1[教师信息管理]
-    D --> D2[教师安排]
-    D --> D3[教师查询]
-
-    E --> E1[成绩录入]
-    E --> E2[成绩查询]
-    E --> E3[成绩统计]
-
-    F --> F1[班级信息管理]
-    F --> F2[班级安排]
-    F --> F3[班级查询]
-\`\`\`
-`;
-
-  constructor(
-    private readonly router: Router,
-    private readonly clipboard: Clipboard,
-    private readonly userService: UserService,
-    private readonly prdService: PrdService,
-  ) {}
-
+  structure = ``;
   mermaidMarkdown =
 `\`\`\`mermaid
 graph TD;
@@ -100,6 +52,15 @@ graph TD;
   B-->D;
   C-->D;
 \`\`\``;
+
+  constructor(
+    private readonly router: Router,
+    private readonly clipboard: Clipboard,
+    private readonly userService: UserService,
+    private readonly prdService: PrdService,
+  ) {}
+
+
   mermaidOptions: MermaidAPI.Config = {
     fontFamily: 'inherit',
     theme: MermaidAPI.Theme.Dark,
@@ -109,28 +70,13 @@ graph TD;
     this.userService.isAuthenticated
       .pipe(
         tap((isAuthenticated) => {
-          if (isAuthenticated) {
-            this.setListTo("feed");
-          } else {
-            this.setListTo("all");
-          }
+
         }),
         takeUntilDestroyed(this.destroyRef),
       )
       .subscribe(
         (isAuthenticated: boolean) => (this.isAuthenticated = isAuthenticated),
       );
-  }
-
-  setListTo(type: string = "", filters: Object = {}): void {
-    // If feed is requested but user is not authenticated, redirect to login
-    if (type === "feed" && !this.isAuthenticated) {
-      void this.router.navigate(["/login"]);
-      return;
-    }
-
-    // Otherwise, set the list object
-    this.listConfig = { type: type, filters: filters };
   }
 
   // 复制内容到剪贴板
